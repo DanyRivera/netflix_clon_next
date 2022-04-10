168885ee7850ffcd52063c22f905d24268267304
Dany <dany@gmail.com>
2022-04-09 20:36:07 -0500

    Empezando con la funcionalidad

---
+++ components/ListadoPeliculas.jsx
@@ -0,0 +1,60 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPlus, faThumbsUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
+import styles from "../styles/ListadoPeliculas.module.css";
+
+const ListadoPeliculas = ({ peliculas, heading, tematica }) => {
+
+    const peliculasArr = peliculas.Search;
+
+    // console.log(peliculasArr);
+
+    return (
+
+        <div className={`${heading ? styles.heading : styles.peliculas}`}>
+
+            <div>
+
+                <h2 className={styles.tematica}>{tematica}</h2>
+
+                <div
+                    className={styles.listado}
+                >
+                    {peliculasArr.map(pelicula => (
+                        <div
+                            key={pelicula.imdbID}
+                            className={styles.pelicula}
+                        >
+                            <Image width={450} height={800} src={pelicula.Poster} />
+
+                            <div className={styles.info}>
+
+                                <div className={styles.opciones}>
+                                    <FontAwesomeIcon className={styles.icon} icon={faPlay} />
+                                    <FontAwesomeIcon className={styles.icon} icon={faPlus} />
+                                    <FontAwesomeIcon className={styles.icon} icon={faThumbsUp} />
+                                    <FontAwesomeIcon className={styles.icon} icon={faChevronDown} />
+                                </div>
+
+
+                                <p>{pelicula.Type}</p>
+
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
+
+            </div>
+
+
+        </div>
+
+    )
+}
+
+ListadoPeliculas.defaultProps = {
+    heading: false
+}
+
+export default ListadoPeliculas
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 import Layout from "../components/Layout"
+import ListadoPeliculas from "../components/ListadoPeliculas";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ peliculasAccion, peliculasAventura, peliculasInfantiles, peliculasTerror }) {
+
+  // console.log(peliculasAccion);
+
   return (
     <Layout
       pagina={'Home'}
@@ -10,23 +16,90 @@ export default function Home() {
 
       <div className={styles.heading}>
 
-
         <div className={styles.imagenSerie}>
 
           <Image width={550} height={350} src="/img/nombre_serie.png" alt="Nombre Serie" />
 
           <div className={styles.acciones}>
-            <button>Play</button>
-            <button>More Info</button>
+
+            <button className={`${styles.boton} ${styles.botonPlay}`}>
+              <div>
+                <FontAwesomeIcon icon={faPlay} />
+              </div>
+              <span>Play</span>
+            </button>
+
+            <button className={styles.boton}>
+              <div>
+                <FontAwesomeIcon icon={faCircleInfo} />
+              </div>
+              <span>More Info</span>
+            </button>
+
           </div>
 
         </div>
 
       </div>
 
+      <main>
 
+        <ListadoPeliculas
+          peliculas={peliculasAccion}
+          heading={true}
+          tematica="Accion"
+        />
 
+        <ListadoPeliculas
+          peliculas={peliculasAventura}
+          tematica="Aventura"
+        />
+
+        <ListadoPeliculas
+          peliculas={peliculasInfantiles}
+          tematica="Infantiles"
+        /> 
+
+        <ListadoPeliculas
+          peliculas={peliculasTerror}
+          tematica="Terror"
+        />  
+
+      </main>
 
     </Layout>
   )
+
+}
+
+export async function getServerSideProps() {
+
+  const urlAccion = `${process.env.API_URL}&s=transformers`;
+  const urlAventura = `${process.env.API_URL}&s=avengers`;
+  const urlInfantiles = `${process.env.API_URL}&s=toy-story`;
+  const urlTerror = `${process.env.API_URL}&s=harry-potter`;
+
+  const [resAccion, resAventura, resInfantiles, resTerror] = await Promise.all([
+    fetch(urlAccion),
+    fetch(urlAventura),
+    fetch(urlInfantiles),
+    fetch(urlTerror)
+  ])
+
+  const [peliculasAccion, peliculasAventura, peliculasInfantiles, peliculasTerror] = await Promise.all([
+    resAccion.json(),
+    resAventura.json(),
+    resInfantiles.json(),
+    resTerror.json()
+  ])
+
+  return {
+    props: {
+      peliculasAccion,
+      peliculasAventura,
+      peliculasInfantiles,
+      peliculasTerror
+    }
+  }
+
 }
